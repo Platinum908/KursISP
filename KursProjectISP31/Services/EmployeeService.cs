307ad8141601d479d2321b540f4f540b0b0e45a4
rakdav <rakdav@inbox.ru>
2025-06-07 13:42:18 +0200

    last7/06

--- KursProjectISP31/Services/EmployeeService.cs
+++ KursProjectISP31/Services/EmployeeService.cs
@@ -2,6 +2,7 @@
 using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,27 +11,39 @@ namespace KursProjectISP31.Services
 {
     public class EmployeeService : BaseService<Employee>
     {
+        public EmployeeService():base()
+        {
+        }
+
         public override bool Add(Employee obj)
         {
             bool IsAdded = false;
             if (obj.Age < 18 || obj.Age > 70)
                 throw new ArgumentException("Не соблюден лимит по возрасту");
-            using (objSqlconnection) 
+            try
             {
                 objSqlCommand.Parameters.Clear();
                 objSqlCommand.CommandText = "udp_InsertEmployee";
-                objSqlCommand.Parameters.AddWithValue("@NameEmp",obj.NameEmp);
+                objSqlCommand.Parameters.AddWithValue("@NameEmp", obj.NameEmp);
                 objSqlCommand.Parameters.AddWithValue("@Age", obj.Age);
                 objSqlconnection.Open();
                 int addRows = objSqlCommand.ExecuteNonQuery();
                 IsAdded = addRows > 0;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                objSqlconnection.Close();
             }
             return IsAdded;
         }
         public override bool Delete(int id)
         {
             bool IsDeleted = false;
-            using (objSqlconnection)
+            try
             {
                 objSqlCommand.Parameters.Clear();
                 objSqlCommand.CommandText = "udp_DeleteEmployee";
@@ -39,13 +52,21 @@ namespace KursProjectISP31.Services
                 int delRows = objSqlCommand.ExecuteNonQuery();
                 IsDeleted = delRows > 0;
             }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                objSqlconnection.Close();
+            }
             return IsDeleted;
         }
         public override List<Employee> GetAll()
         {
             List<Employee> list = new List<Employee>();
-            using (objSqlconnection)
-            {
+            try 
+            { 
                 objSqlCommand.Parameters.Clear();
                 objSqlCommand.CommandText = "udp_SelectAllEmployee";
                 objSqlconnection.Open();
@@ -62,13 +83,22 @@ namespace KursProjectISP31.Services
                         list.Add(objEmployee);
                     }
                 }
-                return list;
+                ObjSqlDataReader.Close();
             }
+            catch(SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                objSqlconnection.Close();
+            }
+            return list;
         }
         public override bool Update(Employee obj)
         {
             bool IsUpdate = false;
-            using (objSqlconnection)
+            try
             {
                 objSqlCommand.Parameters.Clear();
                 objSqlCommand.CommandText = "udp_UpdateEmployee";
@@ -78,6 +108,14 @@ namespace KursProjectISP31.Services
                 objSqlconnection.Open();
                 int updateRows = objSqlCommand.ExecuteNonQuery();
                 IsUpdate = updateRows > 0;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                objSqlconnection.Close();
             }
             return IsUpdate;
         }
